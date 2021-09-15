3671e325dc073d035ab63ffc69e035643015baad
gaitok <88373033+gaitok@users.noreply.github.com>
2021-09-15 15:01:59 +0900

    エラーの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,34 +20,34 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
-    it 'パスワード(確認)が空だと入力できない' do
+    it 'パスワード(確認)が一致しないと入力できない' do
       user = User.new(nickname: 'a', email: 'aaa@aaa', password: 'aaaaa0', password_confirmation: '', first_name: 'あ',
                       last_name: 'あ', first_name_kana: 'ア', last_name_kana: 'ア', birthday: '1931-01-01 00:00:00')
       user.valid?
-      expect(user.errors.full_messages).to include("Password_confirmation can't be blank")
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'お名前(全角)苗字が空だと入力できない' do
-      # user = User.new(nickname: "a", email: "aaa@aaa", password: "aaaaa0", password_confirmation: "aaaaa0", first_name:"",last_name:"あ",first_name_kana:"ア",last_name_kana:"ア",birthday:"1931-01-01 00:00:00")
-      # user.valid?
-      # expect(user.errors.full_messages).to include("First_name can't be blank")
+      user = User.new(nickname: "a", email: "aaa@aaa", password: "aaaaa0", password_confirmation: "aaaaa0", first_name:"",last_name:"あ",first_name_kana:"ア",last_name_kana:"ア",birthday:"1931-01-01 00:00:00")
+      user.valid?
+      expect(user.errors.full_messages).to include("First name can't be blank")
     end
     it 'お名前(全角)名前が空だと入力できない' do
       user = User.new(nickname: 'a', email: 'aaa@aaa', password: 'aaaaa0', password_confirmation: 'aaaaa0', first_name: 'あ',
                       last_name: '', first_name_kana: 'ア', last_name_kana: 'ア', birthday: '1931-01-01 00:00:00')
       user.valid?
-      expect(user.errors.full_messages).to include("Last_name can't be blank")
+      expect(user.errors.full_messages).to include("Last name can't be blank")
     end
     it 'お名前カナ(全角)苗字が空だと入力できない' do
       user = User.new(nickname: 'a', email: 'aaa@aaa', password: 'aaaaa0', password_confirmation: 'aaaaa0', first_name: 'あ',
                       last_name: 'あ', first_name_kana: '', last_name_kana: 'ア', birthday: '1931-01-01 00:00:00')
       user.valid?
-      expect(user.errors.full_messages).to include("First_name_kana can't be blank")
+      expect(user.errors.full_messages).to include("First name kana can't be blank")
     end
     it 'お名前カナ(全角)名前が空だと入力できない' do
       user = User.new(nickname: 'a', email: 'aaa@aaa', password: 'aaaaa0', password_confirmation: 'aaaaa0', first_name: 'あ',
                       last_name: 'あ', first_name_kana: 'ア', last_name_kana: '', birthday: '1931-01-01 00:00:00')
       user.valid?
-      expect(user.errors.full_messages).to include("Last_name_kana can't be blank")
+      expect(user.errors.full_messages).to include("Last name kana can't be blank")
     end
     it '生年月日が空だと入力できない' do
       user = User.new(nickname: 'a', email: 'aaa@aaa', password: 'aaaaa0', password_confirmation: 'aaaaa0', first_name: 'あ',
