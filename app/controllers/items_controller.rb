3598be599cbfa2f67ef634fb586ebec3dab3eda9
gaitok <88373033+gaitok@users.noreply.github.com>
2021-09-16 15:50:21 +0900

    セレクトボックスの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,39 @@
 class ItemsController < ApplicationController
+
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(prototype_params)
+    if @Item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:title, :text, :category_id, :status_id,:price,:postage_id,:place_id,:day_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :place
+  belongs_to :day
 
   with_options presence: true do
   validates :title
   validates :text 
-  validates :category_id
-  validates :status_id
   validates :price,numericality: { in: 300..9999999 }
-  validates :postage_id
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+  validates :category_id 
+  validates :status_id 
+  validates :postage_id 
   validates :place_id
   validates :day_id
   end
+end
 end
