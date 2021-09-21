d6d59f1d5bbb9c0523ac71f8e51722564daf927d
gaitok <88373033+gaitok@users.noreply.github.com>
2021-09-21 19:02:24 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # end
+  def destroy
+    @items = Item.find(params[:id])
+    @items.destroy
+    redirect_to root_path
+  end
 
   private
 
